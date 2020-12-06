1c25216a077fd62491b16a533332866817242a25
Matías Méndez <>
2020-12-06 13:11:36 -0300

    cambios finales

--- main.py
+++ main.py
@@ -51,4 +51,4 @@ def run_one_layout(layout = "mediumGrid"):
             else "Pacman Lose,", "Scores:", game_state.get_rewards())
 
 if __name__ == '__main__':
-    run_one_layout("smallClassic")
+    run_one_layout("smallGrid")
